b9947677aa5f0f36a1626ec12066dc791c5dfa53
CelilSincanli <icelilsincanli@gmail.com>
2023-07-15 01:50:00 +0300

    first commit

---
+++ build/CMakeFiles/voronoi_diagram.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/voronoi_diagram.dir/src/main.cpp.o"
+  "voronoi_diagram"
+  "voronoi_diagram.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/voronoi_diagram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
